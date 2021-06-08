2502eadc3128c7a9db8c758178ba0045ad7187a3
Erich Sauer <erich.sauer@gmail.com>
2021-06-08 13:47:52 -0700

    setup

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,6 @@
-const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
-const request = require('supertest');
-const app = require('../lib/app');
-
 describe('matter-ws-p5 routes', () => {
-  beforeEach(() => {
-    return setup(pool);
+  it('tells us we are cool', () => {
+    const us = 'cool';
+    expect(us).toEqual('cool');
   });
 });
